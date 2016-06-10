fb2e85d915315ab171a4a6c2dd1ba2f42badfb63
toadlyBroodle <toadlybroodle@hotmail.com>
2016-06-10 19:13:50 +0900

    added code snippets for implementing navigation drawer, unable to build, some errors, added extras folder with storeListing.txt

--- app/src/main/java/ca/toadlybroodle/alternaterealitygames/MainActivity.java
+++ app/src/main/java/ca/toadlybroodle/alternaterealitygames/MainActivity.java
@@ -2,12 +2,21 @@ package ca.toadlybroodle.alternaterealitygames;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -21,11 +30,35 @@ public class MainActivity extends AppCompatActivity {
 
     private static LocationActivity mLocationActivity;
 
+    /** drawer implementation copied from example at https://github.com/codepath/android_guides/wiki/Fragment-Navigation-Drawer */
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+        drawerToggle = setupDrawerToggle();
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawer.addDrawerListener(drawerToggle);
+
+        // There is usually only 1 header view.
+        // Multiple header views can technically be added at runtime.
+        // We can use navigationView.getHeaderCount() to determine the total number.
+        // get handel to header layout
+        //View headerLayout = navigationView.getHeaderView(0);
 
         // check for signed in user
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -40,6 +73,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -49,36 +86,117 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+/*    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // ensure
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // The action bar home/up action should open or close the drawer.
         switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
             case R.id.menu_account:
                 // TODO show account page
-                return true;
+                break;
             case R.id.menu_location:
                 // take care of location permissions, settings, and updating
                 if (mLocationActivity == null) {
-                Intent _LocationActivity = new Intent(getApplicationContext(), LocationActivity.class);
-                startActivity(_LocationActivity);
+                    Intent _LocationActivity = new Intent(getApplicationContext(), LocationActivity.class);
+                    startActivity(_LocationActivity);
                 }
-                return true;
+                break;
             case R.id.menu_map:
                 // TODO this is a fragment NOT an activity, have to treat it differently or CRASH
                 Intent _MapsActivity = new Intent(getApplicationContext(), MapsActivity.class);
                 startActivity(_MapsActivity);
-                return true;
+                break;
             case R.id.menu_settings:
                 Intent _SettingsActivity = new Intent(getApplicationContext(), SettingsActivity.class);
                 startActivity(_SettingsActivity);
-                return true;
+                break;
             case R.id.menu_share:
                 // TODO send to share page
-                return true;
+                break;
             case R.id.menu_rate:
                 // TODO send to ratings page
-                return true;
+                break;
             default:
                 return super.onOptionsItemSelected(item);
+
+/**            case R.id.nav_first_fragment:
+                fragmentClass = FirstFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = SecondFragment.class;
+                break;
+            case R.id.nav_third_fragment:
+                fragmentClass = ThirdFragment.class;
+                break;
+            default:
+                fragmentClass = FirstFragment.class; */
         }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE! Make sure to override the method with only a single `Bundle` argument
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
